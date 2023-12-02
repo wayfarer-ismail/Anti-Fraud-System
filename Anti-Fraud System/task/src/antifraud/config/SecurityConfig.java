61489306849a7216d41a0a240ac41fcd5fbd8bc5
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-12-02 21:06:44 +0000

    trying to allow requests by unauthorised users to api

---
+++ Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package antifraud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    public SecurityConfig(RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .csrf((csrf) -> csrf.disable())                           // For modifying requests via Postman
+                .exceptionHandling(handing -> handing
+                        .authenticationEntryPoint(restAuthenticationEntryPoint) // Handles auth error
+                )
+                .headers(headers -> headers.frameOptions().disable())           // for Postman, the H2 console
+                .authorizeHttpRequests(requests -> requests                     // manage access
+                                .requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
+                                .requestMatchers("/actuator/shutdown").permitAll()      // needs to run test
+                        // other matchers
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // no session
+                )
+                // other configurations
+                .build();
+    }
+}
