8159bfaa0e559a140d4d0097a49300c005baaa30
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-12-20 21:10:11 +0000

    made class into record

--- Anti-Fraud System/task/src/antifraud/model/TransactionRequest.java
+++ Anti-Fraud System/task/src/antifraud/model/TransactionRequest.java
@@ -1,21 +1,4 @@
 package antifraud.model;
 
-public class TransactionRequest {
-    private double amount;
-
-    public TransactionRequest() {
-        // Default constructor
-    }
-
-    public TransactionRequest(double amount) {
-        this.amount = amount;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+public record TransactionRequest(double amount) {
 }
