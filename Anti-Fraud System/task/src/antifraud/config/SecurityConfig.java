c8c743e64a2b02a6bf44622850a2b857dc9edfd2
wayfarer-ismail <ismailelbahja03@gmail.com>
2024-01-10 21:41:46 +0000

    organised and added rules for new endpoints

--- Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
+++ Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
@@ -37,19 +37,26 @@ public class SecurityConfig {
                 )
                 .headers(headers -> headers.frameOptions().disable())           // for Postman, the H2 console
                 .authorizeHttpRequests(requests -> requests                     // manage access
-                                .requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
                                 .requestMatchers("/actuator/shutdown").permitAll()      // needs to run test
-                                .requestMatchers(HttpMethod.GET, "/api/auth/list").hasAnyRole("ADMINISTRATOR", "SUPPORT")
-                                .requestMatchers(HttpMethod.POST, "/api/antifraud/transaction").hasRole("MERCHANT")
+                                .requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
                                 .requestMatchers(HttpMethod.DELETE, "/api/auth/user/*").hasRole("ADMINISTRATOR")
+                                .requestMatchers(HttpMethod.GET, "/api/auth/list").hasAnyRole("ADMINISTRATOR", "SUPPORT")
                                 .requestMatchers(HttpMethod.PUT, "/api/auth/access").hasRole("ADMINISTRATOR")
                                 .requestMatchers(HttpMethod.PUT, "/api/auth/role").hasRole("ADMINISTRATOR")
+
+                                .requestMatchers(HttpMethod.POST, "/api/antifraud/transaction").hasRole("MERCHANT")
+                                .requestMatchers(HttpMethod.PUT, "/api/antifraud/transaction").hasRole("SUPPORT")
+
                                 .requestMatchers(HttpMethod.GET, "/api/antifraud/suspicious-ip").hasRole("SUPPORT")
                                 .requestMatchers(HttpMethod.POST, "/api/antifraud/suspicious-ip").hasRole("SUPPORT")
                                 .requestMatchers(HttpMethod.DELETE, "/api/antifraud/suspicious-ip/*").hasRole("SUPPORT")
+
                                 .requestMatchers(HttpMethod.GET, "/api/antifraud/stolencard").hasRole("SUPPORT")
                                 .requestMatchers(HttpMethod.POST, "/api/antifraud/stolencard").hasRole("SUPPORT")
                                 .requestMatchers(HttpMethod.DELETE, "/api/antifraud/stolencard/*").hasRole("SUPPORT")
+
+                                .requestMatchers(HttpMethod.PUT, "/api/antifraud/history").hasRole("SUPPORT")
+
                         // other matchers
                 )
                 .sessionManagement(session -> session
